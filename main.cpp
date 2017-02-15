e103a038b634dda558b078ee6723ca313bf3caad
JoshuaJacobson <fudbuds@gmail.com>
2017-02-14 20:39:44 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,13 @@
 #include <iostream>
 #include "opencv2/opencv.hpp"
-#include "liveMedia/liveMedia.hh"
-#include "BasicUsageEnvironment/BasicUsageEnvironment.hh"
-#include "groupsock/GroupsockHelper.hh"
-#include "groupsock/Groupsock.hh"
-#include "UsageEnvironment/UsageEnvironment.hh"
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <string.h>
-#include <errno.h>
-#include <string.h>
-#include <unistd.h>
+#include <sys/stat.h>
 
 using namespace cv;
 
-UsageEnvironment* env;
-H264VideoStreamFramer* videoSource;
-RTPSink* videoSink;
-
 int main() {
-    /*VideoCapture cap;
+    VideoCapture cap;
+    const char *file = "/var/www/html/cam.jpg";
+    struct stat buffer;
 
     if(!cap.open(0))
         return 0;
@@ -31,76 +17,18 @@ int main() {
 
         cap >> frame;
 
-        frame -= Scalar(255, 200, 255);
+        //frame -= Scalar(255, 200, 255);
 
         if( frame.empty()) break; // end of video stream
+
+        if (stat(file, &buffer)) std::remove(file);
+
+        imwrite(file, frame);
 
         if( waitKey(10) == 27 ) break; // stop capturing by pressing ESC
     }
     // the camera will be closed automatically upon exit
-    // cap.close();*/
-
-    TaskScheduler* scheduler = BasicTaskScheduler::createNew();
-    env = BasicUsageEnvironment::createNew(*scheduler);
-
-    // Create 'groupsocks' for RTP and RTCP:
-    struct in_addr destinationAddress;
-    destinationAddress.s_addr = chooseRandomIPv4SSMAddress(*env);
-    // Note: This is a multicast address.  If you wish instead to stream
-    // using unicast, then you should use the "testOnDemandRTSPServer"
-    // test program - not this test program - as a model.
-
-    const unsigned short rtpPortNum = 18888;
-    const unsigned short rtcpPortNum = rtpPortNum+1;
-    const unsigned char ttl = 255;
-
-    const Port rtpPort(rtpPortNum);
-    const Port rtcpPort(rtcpPortNum);
-
-    Groupsock rtpGroupsock(*env, destinationAddress, rtpPort, ttl);
-    rtpGroupsock.multicastSendOnly(); // we're a SSM source
-    Groupsock rtcpGroupsock(*env, destinationAddress, rtcpPort, ttl);
-    rtcpGroupsock.multicastSendOnly(); // we're a SSM source
-
-    // Create a 'H264 Video RTP' sink from the RTP 'groupsock':
-    OutPacketBuffer::maxSize = 600000;
-    videoSink = H264VideoRTPSink::createNew(*env, &rtpGroupsock, 96);
-
-    // Create (and start) a 'RTCP instance' for this RTP sink:
-    const unsigned estimatedSessionBandwidth = 1024; // in kbps; for RTCP b/w share
-    const unsigned maxCNAMElen = 100;
-    unsigned char CNAME[maxCNAMElen+1];
-    gethostname((char*)CNAME, maxCNAMElen);
-    CNAME[maxCNAMElen] = '\0'; // just in case
-    RTCPInstance* rtcp
-            = RTCPInstance::createNew(*env, &rtcpGroupsock,
-                                      estimatedSessionBandwidth, CNAME,
-                                      videoSink, NULL /* we're a server */,
-                                      True /* we're a SSM source */);
-    // Note: This starts RTCP running automatically
-
-    RTSPServer* rtspServer = RTSPServer::createNew(*env, 8554);
-    if (rtspServer == NULL) {
-        *env << "Failed to create RTSP server: " << env->getResultMsg() << "\n";
-        exit(1);
-    }
-    ServerMediaSession* sms
-            = ServerMediaSession::createNew(*env, "ipcamera","UPP Buffer" ,
-                                            "Session streamed by \"testH264VideoStreamer\"",
-                                            True /*SSM*/);
-    sms->addSubsession(PassiveServerMediaSubsession::createNew(*videoSink, rtcp));
-    rtspServer->addServerMediaSession(sms);
-
-    char* url = rtspServer->rtspURL(sms);
-    *env << "Play this stream using the URL \"" << url << "\"\n";
-    delete[] url;
-
-    // Start the streaming:
-    *env << "Beginning streaming...\n";
-
-    env->taskScheduler().doEventLoop();
-
-
+    // cap.close();
     return 0;
 }
 
